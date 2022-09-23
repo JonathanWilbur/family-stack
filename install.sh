a65f2b139117799db2ef6836f57400dc7b9ce590
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-09-23 19:21:54 -0400

    Fix a few bugs

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt install -y git gpg sendmail mailutils
 openssl req -x509 -sha256 -days 1000 -nodes -out ./meerkat/cert.pem -keyout ./meerkat/key.pem -sub "/CN=$(hostname)"
-cat Caddyfile | sed "s|HOSTNAME|$(hostname)|g" > Caddyfile
-cat mysql-init.sql.tmpl | sed "s|DIRECTORY_USER_PASSWORD|${DIRECTORY_DB_PASSWORD}|g" > mysql-init.sql
-cat mysql-init.sql | sed "s|OWNCLOUD_USER_PASSWORD|${OWNCLOUD_DB_PASSWORD}|g" > mysql-init.sql
-cat postgres-init.sql.tmpl | sed "s|SYNAPSE_USER_PASSWORD|${SYNAPSE_DB_PASSWORD}|g" > postgres-init.sql
-cat postgres-init.sql | sed "s|KEYCLOAK_USER_PASSWORD|${KEYCLOAK_DB_PASSWORD}|g" > postgres-init.sql
-cat ./family-stack.service | sed "s|/CHANGEME|${PWD}|g" > ./family-stack.service
+cat Caddyfile | sed -e "s|HOSTNAME|$(hostname)|g" > Caddyfile
+cat mysql-init.sql.tmpl | sed -e "s|DIRECTORY_USER_PASSWORD|${DIRECTORY_DB_PASSWORD}|g" > mysql-init.sql
+cat mysql-init.sql | sed -e "s|OWNCLOUD_USER_PASSWORD|${OWNCLOUD_DB_PASSWORD}|g" > mysql-init.sql
+cat postgres-init.sql.tmpl | sed -e "s|SYNAPSE_USER_PASSWORD|${SYNAPSE_DB_PASSWORD}|g" > postgres-init.sql
+cat postgres-init.sql | sed -e "s|KEYCLOAK_USER_PASSWORD|${KEYCLOAK_DB_PASSWORD}|g" > postgres-init.sql
+cat ./family-stack.service | sed -e "s|/CHANGEME|${PWD}|g" > ./family-stack.service
 cp ./family-stack.service /etc/systemd/system/family-stack.service
 systemctl enable family-stack
 # Backup every Sunday at 03:13 (system time).
